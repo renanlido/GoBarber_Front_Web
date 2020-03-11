1523fc0d775c69107dcbc052662ea21a18fd06ac
Renan Oliveira <renanlido@yahoo.com.br>
2020-03-11 15:15:21 -0300

    chore - Request login data for api

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -6,12 +6,14 @@ import PropTypes from 'prop-types';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   // NOTE Se ele não estiver logado e a rota for privada,
   // redireciona para rota raiz
